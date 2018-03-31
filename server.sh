4c50e55796db6d3606f19ee39bd77159fc361a93
Jason Hsu <jhsu802701@jasonhsu.com>
2018-03-31 20:19:11 +0000

    Updated server.sh message

--- server.sh
+++ server.sh
@@ -10,10 +10,12 @@ sh build.sh
 echo '**********************'
 echo 'OPEN YOUR WEB BROWSER.'
 echo 'GO TO THE FOLLOWING URL:'
-echo 'http://<IP Address>:4000'
-echo 'If Docker is installed directly in your host OS, the IP address is localhost.'
-echo 'If you are using Docker Machine, use the IP address is probably 192.168.99.100.'
-echo '*******************************************************************************'
+echo 'http://localhost:4000'
+echo 'NOTE: If you are using Docker or Vagrant, the port number may be different.'
+echo '***************************************************************************'
+if [ -f '/home/winner/shared/ports.txt' ]; then
+  cat /home/winner/shared/ports.txt;
+fi
 echo '---------------------------------------'
 echo 'bundle exec jekyll serve --host 0.0.0.0'
 bundle exec jekyll serve --host 0.0.0.0
