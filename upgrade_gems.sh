053c5cd0a8cc26da7347436db3af1e7e47d08045
Jason Hsu <jhsu802701@jasonhsu.com>
2018-04-25 05:38:12 +0000

    Added upgrade_gems.sh

---
+++ upgrade_gems.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Use this script for upgrading gems.
+
+# If the version of a gem is pinned in the Gemfile, you must update
+# the version number specified in that file.
+
+# Do NOT use this script until you have set up this project with the build_fast.sh script.
+
+echo '-------------'
+echo 'bundle update'
+bundle update
+
+echo '----------------------------------------------------------'
+echo 'bundle exec gemsurance --output log/gemsurance_report.html'
+bundle exec gemsurance --output log/gemsurance_report.html
+echo 'The Gemsurance Report is at log/gemsurance_report.html .'
